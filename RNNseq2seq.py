18fa8f0b4e142422523cb8237d10eb8c89bbbf8b
abhijithajith1992 <abhijithmanu@gmail.com>
2019-06-24 17:10:10 +1000

    report comit 1

--- RNNseq2seq.py
+++ RNNseq2seq.py
@@ -1,99 +1,104 @@
-	
 from random import seed
 from random import randint
 from numpy import array
 from math import ceil
 from math import log10
-from math import sqrt
 from numpy import argmax
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
 from keras.layers import TimeDistributed
 from keras.layers import RepeatVector
- 
+
+
 # generate lists of random integers and their sum
 def random_sum_pairs(n_examples, n_numbers, largest):
-	X, y = list(), list()
-	for i in range(n_examples):
-		in_pattern = [randint(1,largest) for _ in range(n_numbers)]
-		out_pattern = sum(in_pattern)
-		X.append(in_pattern)
-		y.append(out_pattern)
-	return X, y
- 
+    X, y = list(), list()
+    for i in range(n_examples):
+        in_pattern = [randint(1, largest) for _ in range(n_numbers)]
+        out_pattern = sum(in_pattern)
+        X.append(in_pattern)
+        y.append(out_pattern)
+    return X, y
+
+
 # convert data to strings
 def to_string(X, y, n_numbers, largest):
-	max_length = n_numbers * ceil(log10(largest+1)) + n_numbers - 1
-	Xstr = list()
-	for pattern in X:
-		strp = '+'.join([str(n) for n in pattern])
-		strp = ''.join([' ' for _ in range(max_length-len(strp))]) + strp
-		Xstr.append(strp)
-	max_length = ceil(log10(n_numbers * (largest+1)))
-	ystr = list()
-	for pattern in y:
-		strp = str(pattern)
-		strp = ''.join([' ' for _ in range(max_length-len(strp))]) + strp
-		ystr.append(strp)
-	return Xstr, ystr
- 
+    max_length = n_numbers * ceil(log10(largest+1)) + n_numbers - 1
+    Xstr = list()
+    for pattern in X:
+        strp = '+'.join([str(n) for n in pattern])
+        strp = ''.join([' ' for _ in range(max_length-len(strp))]) + strp
+        Xstr.append(strp)
+    max_length = ceil(log10(n_numbers * (largest+1)))
+    ystr = list()
+    for pattern in y:
+        strp = str(pattern)
+        strp = ''.join([' ' for _ in range(max_length-len(strp))]) + strp
+        ystr.append(strp)
+    return Xstr, ystr
+
+
 # integer encode strings
 def integer_encode(X, y, alphabet):
-	char_to_int = dict((c, i) for i, c in enumerate(alphabet))
-	Xenc = list()
-	for pattern in X:
-		integer_encoded = [char_to_int[char] for char in pattern]
-		Xenc.append(integer_encoded)
-	yenc = list()
-	for pattern in y:
-		integer_encoded = [char_to_int[char] for char in pattern]
-		yenc.append(integer_encoded)
-	return Xenc, yenc
- 
+    char_to_int = dict((c, i) for i, c in enumerate(alphabet))
+    Xenc = list()
+    for pattern in X:
+        integer_encoded = [char_to_int[char] for char in pattern]
+        Xenc.append(integer_encoded)
+    yenc = list()
+    for pattern in y:
+        integer_encoded = [char_to_int[char] for char in pattern]
+        yenc.append(integer_encoded)
+    return Xenc, yenc
+
+
 # one hot encode
 def one_hot_encode(X, y, max_int):
-	Xenc = list()
-	for seq in X:
-		pattern = list()
-		for index in seq:
-			vector = [0 for _ in range(max_int)]
-			vector[index] = 1
-			pattern.append(vector)
-		Xenc.append(pattern)
-	yenc = list()
-	for seq in y:
-		pattern = list()
-		for index in seq:
-			vector = [0 for _ in range(max_int)]
-			vector[index] = 1
-			pattern.append(vector)
-		yenc.append(pattern)
-	return Xenc, yenc
- 
+    Xenc = list()
+    for seq in X:
+        pattern = list()
+        for index in seq:
+            vector = [0 for _ in range(max_int)]
+            vector[index] = 1
+            pattern.append(vector)
+        Xenc.append(pattern)
+    yenc = list()
+    for seq in y:
+        pattern = list()
+        for index in seq:
+            vector = [0 for _ in range(max_int)]
+            vector[index] = 1
+            pattern.append(vector)
+            yenc.append(pattern)
+    return Xenc, yenc
+
+
 # generate an encoded dataset
 def generate_data(n_samples, n_numbers, largest, alphabet):
-	# generate pairs
-	X, y = random_sum_pairs(n_samples, n_numbers, largest)
-	# convert to strings
-	X, y = to_string(X, y, n_numbers, largest)
-	# integer encode
-	X, y = integer_encode(X, y, alphabet)
-	# one hot encode
-	X, y = one_hot_encode(X, y, len(alphabet))
-	# return as numpy arrays
-	X, y = array(X), array(y)
-	return X, y
- 
+    # generate pairs
+    X, y = random_sum_pairs(n_samples, n_numbers, largest)
+    # convert to strings
+    X, y = to_string(X, y, n_numbers, largest)
+    # integer encode
+    X, y = integer_encode(X, y, alphabet)
+    # one hot encode
+    X, y = one_hot_encode(X, y, len(alphabet))
+    # return as numpy arrays
+    X, y = array(X), array(y)
+    return X, y
+
+
 # invert encoding
 def invert(seq, alphabet):
-	int_to_char = dict((i, c) for i, c in enumerate(alphabet))
-	strings = list()
-	for pattern in seq:
-		string = int_to_char[argmax(pattern)]
-		strings.append(string)
-	return ''.join(strings)
- 
+    int_to_char = dict((i, c) for i, c in enumerate(alphabet))
+    strings = list()
+    for pattern in seq:
+        string = int_to_char[argmax(pattern)]
+        strings.append(string)
+    return ''.join(strings)
+
+
 # define dataset
 seed(1)
 n_samples = 1000
@@ -116,9 +121,9 @@ model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accur
 print(model.summary())
 # train LSTM
 for i in range(n_epoch):
-	X, y = generate_data(n_samples, n_numbers, largest, alphabet)
-	print(i)
-	model.fit(X, y, epochs=1, batch_size=n_batch)
+    X, y = generate_data(n_samples, n_numbers, largest, alphabet)
+    print(i)
+    model.fit(X, y, epochs=1, batch_size=n_batch)
  
 # evaluate on some new patterns
 X, y = generate_data(n_samples, n_numbers, largest, alphabet)
@@ -128,4 +133,4 @@ expected = [invert(x, alphabet) for x in y]
 predicted = [invert(x, alphabet) for x in result]
 # show some examples
 for i in range(20):
-	print('Expected=%s, Predicted=%s' % (expected[i], predicted[i]))
+    print('Expected=%s, Predicted=%s' % (expected[i], predicted[i]))
